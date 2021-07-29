baf81bbac106d2504c0917fa17146a7ca2837b02
gonzalezmassini <gonzalez.massini@gmail.com>
2021-07-29 00:44:45 -0400

    score header done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import Results from './components/Results/Reuslts'
-import Rules from './components/Rules/Rules'
+import Results from './components/Results/Reuslts';
+import Rules from './components/Rules/Rules';
 import ScoreHeader from './components/ScoreHeader/ScoreHeader';
-import Selector from './components/Selector/Selector'
+import Selector from './components/Selector/Selector';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-    rock paper scissors
-    <Results/>
-    <Rules/>
-    <ScoreHeader/>
-    <Selector/>
+    <div className={styles.app}>
+      <ScoreHeader />
+      <Selector />
+      <Results />
+      <Rules />
     </div>
   );
 }
--- src/components/ScoreHeader/ScoreHeader.js
+++ src/components/ScoreHeader/ScoreHeader.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import styles from './ScoreHeader.module.css'
+import React from 'react';
+import styles from './ScoreHeader.module.css';
 const ScoreHeader = () => {
- return (
-  <div className={styles.title}>
-   ScoreHeader
-  </div>
- )
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.gameName}>
+        <p> Rock </p> <p> Paper</p> <p>Scissors</p>
+      </div>
+      <div className={styles.score}>
+        <p className={styles.scoreTitle}> Score</p>
+        <p className={styles.scoreNumber}>0</p>
+      </div>
+    </div>
+  );
+};
 
-export default ScoreHeader
+export default ScoreHeader;
