5f9a3a4cc2b1282d727ad764e8044a47d6ff7666
gonzalezmassini <gonzalez.massini@gmail.com>
2021-08-05 00:59:52 -0400

    results functionality done

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,7 +5,8 @@ import styles from './Game.module.css';
 
 const Game = ({ setScore }) => {
   const [isPlay, setIsPlay] = useState(false);
-  // const [playerSelection, setIsPlayerSelection] = useState('');
+  const [playerSelectionResult, setIsPlayerSelectionResult] = useState('');
+  const [cpuSelectionResult, setCpuSelectionResult] = useState('');
   const cpuSelection = ['scissors', 'paper', 'rock'];
   const [result, setResult] = useState('itsworking');
 
@@ -17,34 +18,53 @@ const Game = ({ setScore }) => {
     if (playerSelection === housePick) {
       setResult('draw');
       setIsPlay((prev) => !prev);
+      setIsPlayerSelectionResult(playerSelection);
+      setCpuSelectionResult(housePick);
     } else if (playerSelection === 'rock' && housePick === 'scissors') {
       setResult('you win');
       setScore((prev) => prev + 1);
       setIsPlay((prev) => !prev);
+      setIsPlayerSelectionResult(playerSelection);
+      setCpuSelectionResult(housePick);
     } else if (playerSelection === 'rock' && housePick === 'paper') {
       setResult('you lose');
       setIsPlay((prev) => !prev);
+      setIsPlayerSelectionResult(playerSelection);
+      setCpuSelectionResult(housePick);
     } else if (playerSelection === 'scissors' && housePick === 'rock') {
       setResult('you lose');
       setIsPlay((prev) => !prev);
+      setIsPlayerSelectionResult(playerSelection);
+      setCpuSelectionResult(housePick);
     } else if (playerSelection === 'scissors' && housePick === 'paper') {
       setResult('you win');
       setScore((prev) => prev + 1);
       setIsPlay((prev) => !prev);
+      setIsPlayerSelectionResult(playerSelection);
+      setCpuSelectionResult(housePick);
     } else if (playerSelection === 'paper' && housePick === 'rock') {
       setResult('you win');
       setScore((prev) => prev + 1);
       setIsPlay((prev) => !prev);
+      setIsPlayerSelectionResult(playerSelection);
+      setCpuSelectionResult(housePick);
     } else if (playerSelection === 'paper' && housePick === 'scissors') {
       setResult('you lose');
       setIsPlay((prev) => !prev);
+      setIsPlayerSelectionResult(playerSelection);
+      setCpuSelectionResult(housePick);
     }
   };
 
   return (
     <div className={styles.container}>
       {isPlay ? (
-        <Results result={result} playResult={playResult} />
+        <Results
+          result={result}
+          playResult={playResult}
+          playerSelectionResult={playerSelectionResult}
+          cpuSelectionResult={cpuSelectionResult}
+        />
       ) : (
         <Selector playResult={playResult} />
       )}
--- src/components/Game/Results/Reuslts.js
+++ src/components/Game/Results/Reuslts.js
@@ -4,16 +4,58 @@ import { ReactComponent as Rock } from '../../../assets/images/icon-rock.svg';
 import { ReactComponent as Scissors } from '../../../assets/images/icon-scissors.svg';
 import { ReactComponent as Paper } from '../../../assets/images/icon-paper.svg';
 
-const Reuslts = ({ playResult, result }) => {
+const Reuslts = ({
+  playResult,
+  result,
+  playerSelectionResult,
+  cpuSelectionResult,
+}) => {
   return (
     <div className={styles.title} onClick={() => playResult()}>
+      {cpuSelectionResult === 'rock' ? (
+        <div>
+          {'the house picked'}
+          <Rock className={styles.rock} onClick={() => playResult('rock')} />
+        </div>
+      ) : null}
+      {cpuSelectionResult === 'scissors' ? (
+        <div>
+          {'the house picked'}
+          <Scissors
+            className={styles.scissors}
+            onClick={() => playResult('scissors')}
+          />
+        </div>
+      ) : null}
+      {cpuSelectionResult === 'paper' ? (
+        <div>
+          {'the house picked'}
+          <Paper className={styles.paper} onClick={() => playResult('paper')} />
+        </div>
+      ) : null}
       {result}
-      <Rock className={styles.rock} onClick={() => playResult('rock')} />
-      <Scissors
-        className={styles.scissors}
-        onClick={() => playResult('scissors')}
-      />
-      <Paper className={styles.paper} onClick={() => playResult('paper')} />
+      {playerSelectionResult === 'rock' ? (
+        <div>
+          {'you picked'}
+          <Rock className={styles.rock} onClick={() => playResult('rock')} />
+        </div>
+      ) : null}
+
+      {playerSelectionResult === 'scissors' ? (
+        <div>
+          {'you picked'}
+          <Scissors
+            className={styles.scissors}
+            onClick={() => playResult('scissors')}
+          />
+        </div>
+      ) : null}
+      {playerSelectionResult === 'paper' ? (
+        <div>
+          {'you picked'}
+          <Paper className={styles.paper} onClick={() => playResult('paper')} />
+        </div>
+      ) : null}
     </div>
   );
 };
